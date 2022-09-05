594bd844f2d2ae1c89123a9147d0de9076b29ab3
zhangshengjie <z.cejay@gmail.com>
2022-09-05 12:58:44 +0800

    update decode call data

--- packages/soul-wallet-lib/src/utils/decodeCallData.ts
+++ packages/soul-wallet-lib/src/utils/decodeCallData.ts
@@ -4,170 +4,87 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-09-02 22:38:58
  * @LastEditors: cejay
- * @LastEditTime: 2022-09-02 23:45:10
+ * @LastEditTime: 2022-09-05 12:47:49
  */
 import axios from 'axios';
 import { readFileSync } from 'fs';
-
-/* 
-address
-address[n][]
-bool
-bool[n][]
-bytes
-bytes[n][]
-bytes1
-bytes1[n][]
-bytes10
-bytes11
-bytes12
-bytes12[]
-bytes13
-bytes14
-bytes14[]
-bytes15
-bytes16
-bytes16[]
-bytes16[][]
-bytes17
-bytes18
-bytes18[]
-bytes19
-bytes2
-bytes2[]
-bytes20
-bytes20[]
-bytes21
-bytes22
-bytes22[5]
-bytes23
-bytes24
-bytes24[]
-bytes25
-bytes26
-bytes26[]
-bytes27
-bytes28
-bytes28[]
-bytes28[2]
-bytes29
-bytes3
-bytes3[]
-bytes3[2]
-bytes30
-bytes30[]
-bytes31
-bytes32
-bytes32[]
-bytes32[n][]
-bytes4
-bytes4[]
-bytes4[n][]
-bytes5
-bytes5[]
-bytes6
-bytes6[]
-bytes7
-bytes7[]
-bytes8
-bytes8[]
-bytes9
-fixed128x18
-fixed168x10
-fixed32x32
-fixed80x80
-int104
-int128
-int128[n][]
-int136
-int16
-int16[n][]
-int160
-int168
-int176[8]
-int192
-int200
-int216
-int232
-int24
-int24[n][]
-int248
-int256
-int256[n][]
-int32
-int32[n][]
-int48
-int48[11]
-int56
-int64
-int64[]
-int64[2]
-int8
-int8[n][]
-int80
-int88
-int88[]
-int96
-string
-string[n][]
-ufixed128x18
-ufixed80x80
-uint104
-uint104[]
-uint112
-uint112[]
-uint120
-uint120[]
-uint128
-uint128[n][]
-uint136
-uint144
-uint152
-uint16
-uint16[n][]
-uint160
-uint160[n][]
-uint168
-uint176
-uint184
-uint192
-uint200
-uint208
-uint216
-uint224
-uint232
-uint24
-uint24[n][]
-uint240
-uint248
-uint248[]
-uint256
-uint256[n][]
-uint32
-uint32[n][]
-uint40
-uint40[n][]
-uint48
-uint48[]
-uint56
-uint64
-uint64[n][]
-uint72
-uint72[]
-uint8
-uint8[n][]
-uint80
-uint88
-uint88[]
-uint96
-uint96[n][]
-*/
+import Web3 from 'web3';
 
 export class DecodeCallData {
     private static instance: DecodeCallData;
+    private bytes4Methods = new Map<string, IByte4Method>();
     private _saveToStorage: ((key: string, value: string) => any) | null = null;
     private _readFromStorage: ((key: string) => string | null) | null = null;
+    private web3 = new Web3();
     private constructor() {
+        /*  
+     0xa9059cbb	transfer(address,uint256)
+     0x095ea7b3	approve(address,uint256)
+     0x23b872dd	transferFrom(address,address,uint256)
+     0xb88d4fde	safeTransferFrom(address,address,uint256,bytes)
+     0x42842e0e	safeTransferFrom(address,address,uint256)
+     0x23b872dd	transferFrom(address,address,uint256)
+     0x095ea7b3	approve(address,uint256)
+     0xa22cb465	setApprovalForAll(address,bool)
+     0xf242432a	safeTransferFrom(address,address,uint256,uint256,bytes)
+     0x2eb2c2d6	safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)
+     0xa22cb465	setApprovalForAll(address,bool)
+             */
+        this.bytes4Methods.set('0xa9059cbb', {
+            functionName: 'transfer',
+            functionSignature: 'transfer(address,uint256)',
+            typesArray: ['address', 'uint256']
+        });
+        this.bytes4Methods.set('0x095ea7b3', {
+            functionName: 'approve',
+            functionSignature: 'approve(address,uint256)',
+            typesArray: ['address', 'uint256']
+        });
+        this.bytes4Methods.set('0x23b872dd', {
+            functionName: 'transferFrom',
+            functionSignature: 'transferFrom(address,address,uint256)',
+            typesArray: ['address', 'address', 'uint256']
+        });
+        this.bytes4Methods.set('0xb88d4fde', {
+            functionName: 'safeTransferFrom',
+            functionSignature: 'safeTransferFrom(address,address,uint256,bytes)',
+            typesArray: ['address', 'address', 'uint256', 'bytes']
+        });
+        this.bytes4Methods.set('0x42842e0e', {
+            functionName: 'safeTransferFrom',
+            functionSignature: 'safeTransferFrom(address,address,uint256)',
+            typesArray: ['address', 'address', 'uint256']
+        });
+        this.bytes4Methods.set('0x23b872dd', {
+            functionName: 'transferFrom',
+            functionSignature: 'transferFrom(address,address,uint256)',
+            typesArray: ['address', 'address', 'uint256']
+        });
+        this.bytes4Methods.set('0x095ea7b3', {
+            functionName: 'approve',
+            functionSignature: 'approve(address,uint256)',
+            typesArray: ['address', 'uint256']
+        });
+        this.bytes4Methods.set('0xa22cb465', {
+            functionName: 'setApprovalForAll',
+            functionSignature: 'setApprovalForAll(address,bool)',
+            typesArray: ['address', 'bool']
+        });
+        this.bytes4Methods.set('0xf242432a', {
+            functionName: 'safeBatchTransferFrom',
+            functionSignature: 'safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)',
+            typesArray: ['address', 'address', 'uint256', 'uint256', 'bytes']
+        });
+        this.bytes4Methods.set('0x2eb2c2d6', {
+            functionName: 'safeBatchTransferFrom',
+            functionSignature: 'safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)',
+            typesArray: ['address', 'address', 'uint256[]', 'uint256[]', 'bytes']
+        });
+        this.bytes4Methods.set('0xa22cb465', {
+            functionName: 'setApprovalForAll',
+            functionSignature: 'setApprovalForAll(address,bool)',
+            typesArray: ['address', 'bool']
+        });
 
 
     }
@@ -203,7 +120,6 @@ export class DecodeCallData {
 
     private async read4BytesMethod(bytes4: string): Promise<string | null> {
         try {
-            bytes4 = bytes4.toLowerCase();
             if (bytes4.length != 10) {
                 return null;
             }
@@ -229,48 +145,92 @@ export class DecodeCallData {
         return null;
     }
 
-    public async decodeSignature(bytes4: string) {
-        // {
-        //     const set = new Set<string>();
-        //     let j1 = await readFileSync('/Users/cejay/Downloads/signatures.json', 'utf8');
-        //     let j2 = j1.split('\n');
-        //     for (let i = 0; i < j2.length; i++) {
-        //         const line = j2[i];
-        //         let i1 = line.indexOf('(');
-        //         let i2 = line.lastIndexOf(')');
-        //         if (i1 > 0 && i2 > i1+1) {
-        //             let a1 = line.substring(i1+1, i2);
-        //             // replace all '(' with ''
-        //             a1 = a1.replace(/\(/g, '').replace(/\)/g, '');
-        //             let r1 = a1.split(',');
-        //             for (let j = 0; j < r1.length; j++) {
-        //                 const item = r1[j];
-        //                 if(!set.has(item)){
-        //                     set.add(item);
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     // print all set 
-        //     for (let item of set) {
-        //         console.log(item);
-        //     } 
-        // }
-        const methodstr = await this.read4BytesMethod(bytes4);
-        if (!methodstr) {
+    /**
+     * decode call data 
+     * @param callData call data
+     * @returns 
+     */
+    public async decode(callData: string): Promise<IDecode | null> {
+        callData = callData.toLowerCase();
+        const bytes4 = callData.slice(0, 10)
+        const method = this.bytes4Methods.get(bytes4);
+        if (method) {
+            const typesArray = method.typesArray;
+            const params = this.web3.eth.abi.decodeParameters(typesArray, callData.slice(10));
+            return {
+                functionName: method.functionName,
+                functionSignature: method.functionSignature,
+                params: params
+            };
+        }
+        const methodSignature = await this.read4BytesMethod(bytes4);
+        if (!methodSignature) {
             return null;
+        } else {
+            const functionName = methodSignature.split('(')[0];
+            return {
+                functionName: functionName,
+                functionSignature: methodSignature,
+                params: null
+            };
         }
-        let index1 = methodstr.indexOf('(');
-        let index2 = methodstr.lastIndexOf(')');
-        if (index1 > 0 && index2 > index1) {
-            const method = methodstr.substring(0, index1);
-            const params = methodstr.substring(index1 + 1, index2);
-            // split 'address,(uint256,uint256)[],int128,(uint256,uint256)[]' to ['address', '(uint256,uint256)[]', 'int128', '(uint256,uint256)[]']
-            // web3.eth.abi.decodeParameter
-            // #TODO
-
-
-        }
-        return null;
     }
+
+
+    // private scanParams(params: string) {
+    //     params = params.replace(/\s/g, '');
+
+    //     const paramCharArr: IParamChar[] = [];
+    //     let tmp_depth = 0;
+    //     let depth_switch = false;
+    //     for (let i = 0; i < params.length; i++) {
+    //         const c = params[i];
+    //         if (c === '(') {
+    //             tmp_depth++;
+    //         } else if (c === ')') {
+    //             depth_switch = true;
+    //         } else if (c === ',') {
+    //             if (depth_switch) {
+    //                 tmp_depth--;
+    //                 depth_switch = false;
+    //             }
+    //         }
+    //         paramCharArr.push({
+    //             char: c,
+    //             depth: tmp_depth
+    //         });
+    //     }
+
+    //     const param: IParam = {
+    //         typeName: 'root',
+    //         subParams: []
+    //     };
+
+    //     //this._scanParams(paramCharArr, param);
+
+    // }
+
+
+}
+
+
+// interface IParamChar {
+//     char: string;
+//     depth: number;
+// }
+// interface IParam {
+//     typeName: string;
+//     subParams: IParam[];
+// }
+
+interface IByte4Method {
+    functionName: string,
+    functionSignature: string;
+    typesArray: string[]
+}
+
+interface IDecode {
+    functionName: string,
+    functionSignature: string;
+    params: any
 }
--- packages/soul-wallet-lib/test/test.ts
+++ packages/soul-wallet-lib/test/test.ts
@@ -4,7 +4,7 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-08-05 19:07:41
  * @LastEditors: cejay
- * @LastEditTime: 2022-09-02 23:45:46
+ * @LastEditTime: 2022-09-05 12:56:40
  */
 import { WalletLib } from '../src/app';
 
@@ -12,13 +12,49 @@ import Web3 from 'web3';
 import { keccak256 } from 'ethers/lib/utils';
 import { Guard } from '../src/utils/guard';
 import { DecodeCallData } from '../src/utils/decodeCallData';
+import { assert } from 'console';
 
 
 async function main() {
 
     const web3 = new Web3();
 
-    //await DecodeCallData.new().decodeSignature('0x57ab89c1');
+
+    // #region decode CallData
+    {
+        {
+            const callDataDecode = await DecodeCallData.new().decode('0x23b872dd00000000000000000000000066fe537df37ca31fab3f350367420c791223f5740000000000000000000000003d77439dd3d1dd1eaa96a95d863433948664ef010000000000000000000000000000000000000000000000000000000000000d71');
+            /*
+                functionName:'transferFrom'
+                functionSignature:'transferFrom(address,address,uint256)'
+                params:{
+                    0:'0x66FE537df37cA31Fab3F350367420C791223F574',
+                    1:'0x3D77439dD3d1dd1EAa96a95D863433948664EF01',
+                    2:'3441'
+                }
+             */
+            assert(callDataDecode != null && callDataDecode.functionName === 'transferFrom' &&
+                callDataDecode.functionSignature === 'transferFrom(address,address,uint256)' &&
+                callDataDecode.params[0] === '0x66FE537df37cA31Fab3F350367420C791223F574' &&
+                callDataDecode.params[1] === '0x3D77439dD3d1dd1EAa96a95D863433948664EF01' &&
+                callDataDecode.params[2] === '3441'
+                , 'decode call data failed');
+        } {
+            const callDataDecode = await DecodeCallData.new().decode('0xfb0f3ee100000000000000000000');
+            /*
+                functionName:'cryethereum_please_fix_collisions_sqyq4k4l5'
+                functionSignature:'cryethereum_please_fix_collisions_sqyq4k4l5()'
+                params:null
+             */
+            assert(callDataDecode != null && callDataDecode.functionName === 'cryethereum_please_fix_collisions_sqyq4k4l5' &&
+                callDataDecode.functionSignature === 'cryethereum_please_fix_collisions_sqyq4k4l5()' &&
+                callDataDecode.params === null
+                , 'decode call data failed');
+        }
+    }
+    // #endregion
+
+
 
     // #region Guard
     {
