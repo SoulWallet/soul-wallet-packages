d387ce0c9c401f3839e73a5e79e21e1035f39b74
rocker <rocker@gmail.com>
2022-09-12 02:08:25 +0800

    added new assets

--- packages/soul-wallet-plugin/src/config/index.ts
+++ packages/soul-wallet-plugin/src/config/index.ts
@@ -1,3 +1,25 @@
+import IconETH from "@src/assets/tokens/eth.svg";
+import IconUSDC from "@src/assets/tokens/usdc.png";
+import IconUSDT from "@src/assets/tokens/usdt.png";
+
+export const assetsList = [
+    {
+        icon: IconETH,
+        symbol: "ETH",
+        balance: "28.1937",
+    },
+    {
+        icon: IconUSDC,
+        symbol: "USDC",
+        balance: "100.6913",
+    },
+    {
+        icon: IconUSDT,
+        symbol: "USDT",
+        balance: "73.9712",
+    },
+];
+
 export default {
     walletName: "Soul Wallet",
     safeCenterURL: "https://google.com",
--- packages/soul-wallet-plugin/src/pages/Wallet/comp/Operations/comp/Assets/index.tsx
+++ packages/soul-wallet-plugin/src/pages/Wallet/comp/Operations/comp/Assets/index.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import IconETH from "@src/assets/tokens/eth.svg";
 import IconChevronRight from "@src/assets/chevron-right.svg";
+import { assetsList } from "@src/config";
 
-const assetsList = [
-    {
-        icon: IconETH,
-        symbol: "ETH",
-        balance: "100000",
-    },
-];
 
 export default function Assets() {
     return (
         <div>
             {assetsList.map((item) => (
-                <div className="flex items-center justify-between py-5 px-3 cursor-pointer hover:bg-gray-100">
+                <div key={item.symbol} className="flex items-center justify-between py-5 px-3 cursor-pointer hover:bg-gray-100">
                     <div className="flex items-center gap-2">
                         <img src={item.icon} className="w-10" />
                         <div className="text-sm">
