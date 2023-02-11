7984c9f07b2a0184d5262ca3dfb2e6345db6da01
rocker <exrobbie@gmail.com>
2023-02-11 22:49:08 +0800

    style: added activity icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Switch from "../Switch";
 import IconInfo from "../../assets/icons/info.svg";
 import IconHelp from "../../assets/icons/help.svg";
+import config from "@src/config";
 
 export default function Footer() {
     const [enabled, setEnabled] = useState<boolean>(false);
@@ -9,10 +10,16 @@ export default function Footer() {
         <div className="px-6 py-3 flex items-center justify-between absolute bottom-0 left-0 right-0 footer-shadow">
             <div className="flex items-center gap-[6px]">
                 <Switch checked={enabled} onChange={setEnabled} />
-                <img src={IconInfo} className="w-4 h-4" />
+                <div
+                    className="cursor-pointer tooltip"
+                    data-tip={"Hello world"}
+                >
+                    <img src={IconInfo} className="w-4 h-4" />
+                </div>
             </div>
-
-            <img src={IconHelp} className="w-6 h-6" />
+            <a href={config.socials.telegram} target="_blank">
+                <img src={IconHelp} className="w-6 h-6" />
+            </a>
         </div>
     );
 }
--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import IconActive from "../../assets/icons/switch-active.svg";
-import IconInactive from "../../assets/icons/switch-inactive.svg";
+import cn from "classnames";
 import { ISwitchProps } from "@src/types/ISwitch";
 
 export default function Switch({ checked, onChange }: ISwitchProps) {
     return (
-        <img
-            src={checked ? IconActive : IconInactive}
-            className="cursor-pointer"
+        <div
             onClick={() => onChange(!checked)}
-        />
+            className={cn(
+                "rounded-xl flex p-[2px] cursor-pointer w-[42px]",
+                checked ? "bg-blue justify-end" : "bg-[#999]",
+            )}
+        >
+            <div className="w-5 h-5 rounded-full switch-circle-shadow bg-white" />
+        </div>
     );
 }
