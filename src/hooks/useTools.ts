15c0c830f039e6da4b1bfee1f5f61de67705df6a
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2023-02-19 21:30:28 +0800

    test merge

--- src/hooks/useTools.ts
+++ src/hooks/useTools.ts
@@ -12,6 +12,7 @@ export default function useTools() {
             guardianList,
             Math.round(guardianList.length / 2),
             config.guardianSalt,
+            
         );
     };
 
