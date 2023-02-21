383996115e8110e1597bdceb45ae419287bfc806
rocker <exrobbie@gmail.com>
2023-02-21 08:55:30 +0800

    style: optimized btn style

--- src/components/AccountInfo/index.tsx
+++ src/components/AccountInfo/index.tsx
@@ -74,7 +74,7 @@ export default function AccountInfo({ account, action }: IProps) {
 
             {action === "activate" && walletType === "eoa" && (
                 <div className="px-6 py-4 w-full">
-                    <Button type={"primary"} className="h-auto" onClick={doActivate} loading={loading}>
+                    <Button type={"primary"} onClick={doActivate} loading={loading}>
                         Activate wallet
                     </Button>
                 </div>
