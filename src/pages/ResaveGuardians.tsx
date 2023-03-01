33f3e8428d8a458713948961b552f8d1c5a8d406
rocker <exrobbie@gmail.com>
2023-03-01 11:14:29 +0800

    feat: optimized

--- src/pages/ResaveGuardians.tsx
+++ src/pages/ResaveGuardians.tsx
@@ -2,29 +2,33 @@ import Button from "@src/components/Button";
 import FullscreenContainer from "@src/components/FullscreenContainer";
 import GuardiansSaver from "@src/components/GuardiansSaver";
 import useBrowser from "@src/hooks/useBrowser";
-import { toast } from "material-react-toastify";
 import React, { useState } from "react";
 
 const ResaveGuardians = () => {
     const [saved, setSaved] = useState(false);
-    const { replaceCurrentTab } = useBrowser();
+    // const { replaceCurrentTab } = useBrowser();
+    const { goPlugin } = useBrowser();
 
     const handleSaved = () => {
         setSaved(true);
     };
-    const handleNext = () => {
-        toast.success("Guardians saved successfully! Redirecting to the launch page...");
+    // const handleNext = () => {
+    //     toast.success("Guardians saved successfully! Redirecting to the launch page...");
 
-        setTimeout(() => {
-            // goWebsite("/launch");
-            replaceCurrentTab("/launch");
-        }, 2000);
+    //     setTimeout(() => {
+    //         // goWebsite("/launch");
+    //         replaceCurrentTab("/launch");
+    //     }, 2000);
+    // };
+
+    const handleToMyWallet = () => {
+        goPlugin();
     };
 
     return (
         <FullscreenContainer>
             <div className="flex flex-col pb-14">
-                <h1 className="text-lg text-black font-bold">Save your new guardians</h1>
+                <h1 className="text-lg text-black font-bold">Edit successfully, save your new guardians</h1>
                 <p className="tip-text my-4">
                     Congratulations! You have updated your guardians. We strongly recommend you to make sure you have
                     the copy saved.
@@ -32,8 +36,8 @@ const ResaveGuardians = () => {
 
                 <GuardiansSaver onSave={handleSaved} />
 
-                <Button className="w-base mt-14 mx-auto" type="primary" disabled={!saved} onClick={handleNext}>
-                    Next
+                <Button className="w-base mt-14 mx-auto" type="primary" disabled={!saved} onClick={handleToMyWallet}>
+                    See My Wallet
                 </Button>
             </div>
         </FullscreenContainer>
