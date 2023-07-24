a09fbb18ba653eeb5bb7741aaf79b016dc4e5155
Terence Ge <terencegehui@yahoo.com>
2023-07-24 11:31:39 +0800

    resize logo

--- src/components/LogoV2/index.tsx
+++ src/components/LogoV2/index.tsx
@@ -5,9 +5,9 @@ import classNames from "classnames";
 
 export default function LogoV2({ classname }: { classname?: string }) {
     return (
-        <div className={classNames("flex flex-row h-88 items-center justify-self-center", classname)}>
-            <img src={Logo} className="w-58 h-88" />
-            <img src={LogoText} className="ml-4 w-156 h-23 " />
-        </div>
+      <div className={classNames("logo flex flex-row h-58 items-center justify-self-center", classname)}>
+        <img src={Logo} className="logo-icon w-58 h-58" />
+        <img src={LogoText} className="logo-text ml-4 w-156 h-23 " />
+      </div>
     );
 }
