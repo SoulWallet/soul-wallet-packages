de1e76894050affac027e45b1ba65a74be758d2a
Terence Ge <terencegehui@yahoo.com>
2023-07-24 11:20:21 +0800

    add card ui

--- src/pages/v1/Launch.tsx
+++ src/pages/v1/Launch.tsx
@@ -6,109 +6,87 @@ import { StepActionTypeEn, useStepDispatchContext, CreateStepEn, RecoverStepEn }
 import useBrowser from "@src/hooks/useBrowser";
 import IconClose from '@src/assets/icons/close.svg';
 import { getLocalStorage, setLocalStorage } from "@src/lib/tools";
+import { Center, Flex } from "@chakra-ui/react";
+import CreateWalletIcon from "@src/components/Icons/CreateWallet";
+import RecoverWalletIcon from "@src/components/Icons/RecoverWallet";
 import classNames from "classnames";
 import React, { useEffect, useState } from "react";
 
 export default function Launch() {
-    const [authorized, setAuthorized] = useState(false);
-    const dispatch = useStepDispatchContext();
-    const { goWebsite } = useBrowser();
-    const [showModal, setShowModal] = useState(false);
+  const [authorized, setAuthorized] = useState(false);
+  const dispatch = useStepDispatchContext();
+  const { goWebsite } = useBrowser();
+  const [showModal, setShowModal] = useState(false);
 
-    const getAuthorized = async () => {
-        const authorizedStatus = (await getLocalStorage(AUTHORIZED_STORAGE_KEY)) ?? false;
-        setAuthorized(authorizedStatus);
-        setShowModal(!authorizedStatus);
-    };
+  const getAuthorized = async () => {
+    const authorizedStatus = (await getLocalStorage(AUTHORIZED_STORAGE_KEY)) ?? false;
+    setAuthorized(authorizedStatus);
+    setShowModal(!authorizedStatus);
+  };
 
-    useEffect(() => {
-        getAuthorized();
-    }, []);
+  useEffect(() => {
+    getAuthorized();
+  }, []);
 
-    const handleOpenModal = () => setShowModal(true);
-    const handleCloseModal = () => setShowModal(false);
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
-    const handleAuthorize = () => {
-        setLocalStorage(AUTHORIZED_STORAGE_KEY, true);
-        setAuthorized(true);
-        handleCloseModal();
-    };
+  const handleAuthorize = () => {
+    setLocalStorage(AUTHORIZED_STORAGE_KEY, true);
+    setAuthorized(true);
+    handleCloseModal();
+  };
 
-    const handleJumpToTargetStep = (targetStep: number) => {
-        if (authorized) {
-            dispatch({
-                type: StepActionTypeEn.JumpToTargetStep,
-                payload: targetStep,
-            });
-        }
-    };
+  const handleJumpToTargetStep = (targetStep: number) => {
+    if (authorized) {
+      dispatch({
+        type: StepActionTypeEn.JumpToTargetStep,
+        payload: targetStep,
+      });
+    }
+  };
 
-    return (
-        <FullscreenContainer>
-            <div className="w-[460px] flex flex-col justify-center items-center px-12 py-10">
-                <Button
-                    disabled={!authorized}
-                    href="/popup.html#/v1/create?mode=web"
-                    className="w-full btn btn-purple btn-purple-primary"
-                    onClick={() => handleJumpToTargetStep(CreateStepEn.CreatePWD)}
-                >
-                    Create Wallet
-                </Button>
-
-                <Button
-                    // href="/popup.html#/v1/create?mode=web"
-                    disabled={true} // disable import wallet button for now
-                    className="btn w-full btn-purple mt-5 mb-4"
-                    onClick={() => handleJumpToTargetStep(RecoverStepEn.ResetPassword)}
-                >
-                    Import Wallet
-                </Button>
-
-                <div className="text-gray70">
-                    Lost your wallet?
-                    <span
-                        className={classNames(
-                            "font-bold text-purple ml-1",
-                            authorized ? "cursor-pointer" : "cursor-not-allowed opacity-70",
-                        )}
-                        onClick={() => {
-                            goWebsite("/recover");
-                            handleJumpToTargetStep(RecoverStepEn.ResetPassword);
-                        }}
-                    >
-                        Recover it here
-                    </span>
-                </div>
-
-                <span
-                    className="text-gray70 text-xs cursor-pointer underline underline-offset-4  mt-4"
-                    onClick={handleOpenModal}
-                >
-                    Risk Disclosure Statement
-                </span>
-            </div>
-
-            {/* Risk Disclosure Statement Modal */}
-            <ModalV2 visible={showModal} className="bg-white min-w-[800px] relative">
-                <img src={IconClose} className="absolute right-6 top-6 cursor-pointer" onClick={handleCloseModal} />
-                <div className="flex flex-col items-center gap-4 px-2">
-                    <Statement />
-                    {!authorized ? (
-                        <>
-                            <Button onClick={handleAuthorize} type="primary" className="w-base">
-                                I Understand
-                            </Button>
-                            <a className="skip-text mb-2" onClick={handleCloseModal}>
-                                No, thanks
-                            </a>
-                        </>
-                    ) : (
-                        <span onClick={handleCloseModal} className="cursor-pointer">
-                            Thank you for your agreement.
-                        </span>
-                    )}
-                </div>
-            </ModalV2>
-        </FullscreenContainer>
-    );
+  return (
+    <FullscreenContainer>
+      <Flex w="460px" direction="column" alignItems="center" padding="10px">
+        <div
+          className="w-full btn-card-primary"
+          onClick={() => handleJumpToTargetStep(CreateStepEn.CreatePWD)}
+        >
+          <div><CreateWalletIcon /></div>
+          <div className="w-full card-title">Create New Wallet</div>
+          <div className="w-full card-text">Begin your Web3 journey by creating a smart contract wallet with us.</div>
+        </div>
+        <div
+          className="w-full btn-card"
+          onClick={() => handleJumpToTargetStep(CreateStepEn.CreatePWD)}
+        >
+          <div><RecoverWalletIcon /></div>
+          <div className="w-full card-title">Social Recover</div>
+          <div className="w-full card-text">Social recovery help you retrieve wallets easily with guardian signatures.</div>
+        </div>
+      </Flex>
+      {/* Risk Disclosure Statement Modal */}
+      <ModalV2 visible={showModal} className="bg-white min-w-[800px] relative">
+        <img src={IconClose} className="absolute right-6 top-6 cursor-pointer" onClick={handleCloseModal} />
+        <div className="flex flex-col items-center gap-4 px-2">
+          <Statement />
+          {!authorized ? (
+            <>
+              <Button onClick={handleAuthorize} type="primary" className="w-base">
+                I Understand
+              </Button>
+              <a className="skip-text mb-2" onClick={handleCloseModal}>
+                No, thanks
+              </a>
+            </>
+          ) : (
+            <span onClick={handleCloseModal} className="cursor-pointer">
+              Thank you for your agreement.
+            </span>
+          )}
+        </div>
+      </ModalV2>
+    </FullscreenContainer>
+  );
 }
