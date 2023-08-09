ea234d6deaadbc736fb2fca4578c75d28eea5689
Terence Ge <terencegehui@yahoo.com>
2023-08-09 10:46:46 +0800

    feat: add pin icon, change success background

--- src/pages/v1/Create/SetWalletSuccess/index.tsx
+++ src/pages/v1/Create/SetWalletSuccess/index.tsx
@@ -13,6 +13,8 @@ import Heading3 from "@src/components/web/Heading3";
 import TextBody from "@src/components/web/TextBody";
 import LogoIcon from "@src/assets/logo-v3.svg";
 import CurveArrowIcon from "@src/components/Icons/CurveArrow";
+import BrowserPinIcon from "@src/assets/pinIcon.png";
+import BackgroundImage from "@src/assets/success-background.png";
 import PinIcon from "@src/components/Icons/Pin";
 
 const defaultOptions = {
@@ -43,17 +45,21 @@ const SetWalletSuccess = ({ mode }: IStepCompletion) => {
           Activate Wallet
         </Button>
         {createPortal(
-          <Lottie
-            options={defaultOptions}
-            height="100%"
-            width="100%"
-          />, (document.getElementById('animation-portal')) as any
+          <Box
+            width="100hv"
+            height="100wv"
+          >
+            <Image
+              width="100%"
+              src={BackgroundImage}
+            />
+          </Box>, (document.getElementById('animation-portal')) as any
         )}
         <Box
           position="fixed"
           width="390px"
           height="124px"
-          top="30px"
+          top="50px"
           right="30px"
           background="radial-gradient(51.95% 100% at 100% 100%, #A3B2FF 0%, #E2FC89 100%), linear-gradient(113.16deg, rgba(244, 255, 176, 0.8) 2.41%, rgba(182, 255, 108, 0.8) 76.58%)"
           boxShadow="0px 4.87874698638916px 9.75749397277832px 0px #0000001F"
@@ -71,14 +77,21 @@ const SetWalletSuccess = ({ mode }: IStepCompletion) => {
             <Box
               position="absolute"
               right="50px"
-              top="-40px"
+              top="-60px"
+            >
+              <Image src={BrowserPinIcon} />
+            </Box>
+            <Box
+              position="absolute"
+              right="50px"
+              top="-30px"
             >
               <CurveArrowIcon />
             </Box>
             <Box
               position="absolute"
-              right="-30px"
-              top="-30px"
+              right="-15px"
+              top="-15px"
             >
               <PinIcon />
             </Box>
