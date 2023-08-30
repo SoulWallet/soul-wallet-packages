6d29067568e7edefc657587aa049c63de615827d
rocker <exrobbie@gmail.com>
2023-08-30 17:17:10 +0800

    chore: update api route

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -46,7 +46,7 @@ const guardian = {
 
 const balance = {
     nft: (params: any) => axio.get("/nft-balance", { params }),
-    token: (params: any) => axio.post("/token/erc20", params),
+    token: (params: any) => axio.post("/token/ft", params),
 };
 
 const sponsor = {
